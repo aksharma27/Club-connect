daf64299b28c3a964cdbfeefaf9707857a3a86ed
abhishek <aksharma.27mjm@gmail.com>
2024-04-26 02:39:58 +0530

    basic server setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// DEPENDENCIES : 
+const express  = require('express');
+const dotenv = require('dotenv');
+
+
+//FILES & FOLDERS & ENVS:
+const port = process.env.PORT | 5000; 
+
+
+
+// MIDDLEWARES & OTHER FUNCTIONS : 
+const app = express();
+
+
+
+// API - END POINTS : 
+app.get('/', (req, res) => {
+    res.send('Welcome to club connect home page');
+});
+
+
+
+app.listen(port, () => {
+    console.log("server listening on port " + port);
+});
